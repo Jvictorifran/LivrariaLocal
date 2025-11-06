ada752488df7ae41131678c79ba3c29dd0ac42ec
Jvictorifran <joaovictorcandidoifran@gmail.com>
2025-11-05 20:05:06 -0400

    entendendo sobre django admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from catalog.models import Author, Genre, Book, BookInstance
 
-# Register your models here.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+
+
+class AutorAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Author, AutorAdmin)
